fefc13b50163cb19d25179b0522a5bdcd9fbac2f
buddhiwickramanayaka <buddhiwickramanayaka6@gmail.com>
2021-09-12 08:27:49 +0530

    link and db connect through pages

--- inventory/app.py
+++ inventory/app.py
@@ -89,3 +89,125 @@ def summary():
         print(msg)
 
     return render('index.html', link=link, title="Summary", warehouses=warehouse, products=products, database=q_data)
+
+
+@app.route('/product', methods=['POST', 'GET'])
+def product():
+    init_database()
+    msg = None
+    db = sqlite3.connect(DATABASE_NAME)
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM products")
+    products = cursor.fetchall()
+
+    if request.method == 'POST':
+        prod_name = request.form['prod_name']
+        quantity = request.form['prod_quantity']
+
+        transaction_allowed = False
+        if prod_name not in ['', ' ', None]:
+            if quantity not in ['', ' ', None]:
+                transaction_allowed = True
+
+        if transaction_allowed:
+            try:
+                cursor.execute("INSERT INTO products (prod_name, prod_quantity) VALUES (?, ?)", (prod_name, quantity))
+                db.commit()
+            except sqlite3.Error as e:
+                msg = f"An error occurred: {e.args[0]}"
+            else:
+                msg = f"{prod_name} added successfully"
+
+            if msg:
+                print(msg)
+
+            return redirect(url_for('product'))
+
+    return render('product.html',
+                  link=link, products=products, transaction_message=msg,
+                  title="Products Log")
+
+
+@app.route('/location', methods=['POST', 'GET'])
+def location():
+    init_database()
+    msg = None
+    db = sqlite3.connect(DATABASE_NAME)
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM location")
+    warehouse_data = cursor.fetchall()
+
+    if request.method == 'POST':
+        warehouse_name = request.form['warehouse_name']
+
+        transaction_allowed = False
+        if warehouse_name not in ['', ' ', None]:
+            transaction_allowed = True
+
+        if transaction_allowed:
+            try:
+                cursor.execute("INSERT INTO location (loc_name) VALUES (?)", (warehouse_name,))
+                db.commit()
+            except sqlite3.Error as e:
+                msg = f"An error occurred: {e.args[0]}"
+            else:
+                msg = f"{warehouse_name} added successfully"
+
+            if msg:
+                print(msg)
+
+            return redirect(url_for('location'))
+
+    return render('location.html',
+                  link=link, warehouses=warehouse_data, transaction_message=msg,
+                  title="Warehouse Locations")
+
+
+@app.route('/movement', methods=['POST', 'GET'])
+def movement():
+    init_database()
+    msg = None
+    db = sqlite3.connect(DATABASE_NAME)
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM logistics")
+    logistics_data = cursor.fetchall()
+
+    # add suggestive content for page
+    cursor.execute("SELECT prod_id, prod_name, unallocated_quantity FROM products")
+    products = cursor.fetchall()
+
+    cursor.execute("SELECT loc_id, loc_name FROM location")
+    locations = cursor.fetchall()
+
+    log_summary = []
+    for p_id in [x[0] for x in products]:
+        cursor.execute("SELECT prod_name FROM products WHERE prod_id = ?", (p_id, ))
+        temp_prod_name = cursor.fetchone()
+
+        for l_id in [x[0] for x in locations]:
+            cursor.execute("SELECT loc_name FROM location WHERE loc_id = ?", (l_id,))
+            temp_loc_name = cursor.fetchone()
+
+            cursor.execute("""
+            SELECT SUM(log.prod_quantity)
+            FROM logistics log
+            WHERE log.prod_id = ? AND log.to_loc_id = ?
+            """, (p_id, l_id))
+            sum_to_loc = cursor.fetchone()
+
+            cursor.execute("""
+            SELECT SUM(log.prod_quantity)
+            FROM logistics log
+            WHERE log.prod_id = ? AND log.from_loc_id = ?
+            """, (p_id, l_id))
+            sum_from_loc = cursor.fetchone()
+
+            if sum_from_loc[0] is None:
+                sum_from_loc = (0,)
+            if sum_to_loc[0] is None:
+                sum_to_loc = (0,)
+
+            log_summary += [(temp_prod_name + temp_loc_name + (sum_to_loc[0] - sum_from_loc[0],))]
