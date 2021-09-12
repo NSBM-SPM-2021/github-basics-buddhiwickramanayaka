452277faad3a96dc3d773caf97f0b7ec4e03a5a8
buddhiwickramanayaka <buddhiwickramanayaka6@gmail.com>
2021-09-12 07:47:47 +0530

    initialize page content (products)

--- inventory/app.py
+++ inventory/app.py
@@ -17,3 +17,29 @@ app.config.from_mapping(
     DATABASE=os.path.join(app.instance_path, 'database', DATABASE_NAME),
 )
 
+# listing views
+link = {x: x for x in ["location", "product", "movement"]}
+link["index"] = '/'
+
+
+def init_database():
+    db = sqlite3.connect(DATABASE_NAME)
+    cursor = db.cursor()
+
+    # initialize page content
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS products(prod_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    prod_name TEXT UNIQUE NOT NULL,
+                    prod_quantity INTEGER NOT NULL,
+                    unallocated_quantity INTEGER);
+    """)
+    cursor.execute("""
+    CREATE TRIGGER IF NOT EXISTS default_prod_qty_to_unalloc_qty
+                    AFTER INSERT ON products
+                    FOR EACH ROW
+                    WHEN NEW.unallocated_quantity IS NULL
+                    BEGIN 
+                        UPDATE products SET unallocated_quantity  = NEW.prod_quantity WHERE rowid = NEW.rowid;
+                    END;
+
+    """)
