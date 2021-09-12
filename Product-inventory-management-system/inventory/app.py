1a1946f575b14c7d2b7e7799373166ba378725e1
buddhiwickramanayaka <buddhiwickramanayaka6@gmail.com>
2021-09-12 22:45:55 +0530

    Import to maintain consistency (Bugs fixed)

--- Product-inventory-management-system/inventory/app.py
+++ Product-inventory-management-system/inventory/app.py
@@ -211,3 +211,68 @@ def movement():
                 sum_to_loc = (0,)
 
             log_summary += [(temp_prod_name + temp_loc_name + (sum_to_loc[0] - sum_from_loc[0],))]
+            
+    alloc_json = {}
+    for row in log_summary:
+        try:
+            if row[1] in alloc_json[row[0]].keys():
+                alloc_json[row[0]][row[1]] += row[2]
+            else:
+                alloc_json[row[0]][row[1]] = row[2]
+        except (KeyError, TypeError):
+            alloc_json[row[0]] = {}
+            alloc_json[row[0]][row[1]] = row[2]
+    alloc_json = json.dumps(alloc_json)
+
+    if request.method == 'POST':
+        # transaction times are stored in UTC
+        prod_name = request.form['prod_name']
+        from_loc = request.form['from_loc']
+        to_loc = request.form['to_loc']
+        quantity = request.form['quantity']
+
+        # if no 'from loc' is given, that means the product is being shipped to a warehouse (init condition)
+        if from_loc in [None, '', ' ']:
+            try:
+                cursor.execute("""
+                    INSERT INTO logistics (prod_id, to_loc_id, prod_quantity) 
+                    SELECT products.prod_id, location.loc_id, ? 
+                    FROM products, location 
+                    WHERE products.prod_name == ? AND location.loc_name == ?
+                """, (quantity, prod_name, to_loc))
+
+                # IMPORTANT to maintain consistency
+                cursor.execute("""
+                UPDATE products 
+                SET unallocated_quantity = unallocated_quantity - ? 
+                WHERE prod_name == ?
+                """, (quantity, prod_name))
+                db.commit()
+
+            except sqlite3.Error as e:
+                msg = f"An error occurred: {e.args[0]}"
+            else:
+                msg = "Transaction added successfully"
+
+        elif to_loc in [None, '', ' ']:
+            print("To Location wasn't specified, will be unallocated")
+            try:
+                cursor.execute("""
+                INSERT INTO logistics (prod_id, from_loc_id, prod_quantity) 
+                SELECT products.prod_id, location.loc_id, ? 
+                FROM products, location 
+                WHERE products.prod_name == ? AND location.loc_name == ?
+                """, (quantity, prod_name, from_loc))
+
+                # IMPORTANT to maintain consistency
+                cursor.execute("""
+                UPDATE products 
+                SET unallocated_quantity = unallocated_quantity + ? 
+                WHERE prod_name == ?
+                """, (quantity, prod_name))
+                db.commit()
+
+            except sqlite3.Error as e:
+                msg = f"An error occurred: {e.args[0]}"
+            else:
+                msg = "Transaction added successfully"
